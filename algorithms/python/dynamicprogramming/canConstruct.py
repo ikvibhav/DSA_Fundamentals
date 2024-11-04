b6acd6cad21ee7498851471867e441a3d17b2470
vibhav <ikvibhav@gmail.com>
2024-11-04 22:16:03 +0100

    LRNG-ALGO: 04112024 changes

--- algorithms/python/dynamicprogramming/canConstruct.py
+++ algorithms/python/dynamicprogramming/canConstruct.py
@@ -3,7 +3,9 @@
 # You may reuse elements of `wordBank` as many times as needed.
 
 # Time Complexity: O(n^m * m)
-## Reason: n^m for the number of calls and m for the slicing operation
+## Reason: n^m for the number of calls in the recursion tree and m for the slicing operation
+## slicing operation takes m time because slicing a string of length m takes O(m) time
+## During slicing operation, a new string of length m is created
 # Space Complexity: O(m^2)
 ## Reason: Call stack has m elements and the slicing operation takes m space
 def can_construct(target: str, word_bank: list[str]) -> bool:
@@ -19,3 +21,26 @@ def can_construct(target: str, word_bank: list[str]) -> bool:
 
     return False
 
+
+
+# Time Complexity: O(n * m^2)
+## Reason: n for the number of calls in the recursion tree. m^2 for the slicing operation and memoization
+# Space Complexity: O(m^2)
+## Reason: Call stack has m elements and the slicing operation takes m space
+def can_construct_memo(target, word_bank, memo = {}):
+    if target in memo:
+        return memo[target]
+    
+    if target == '':
+        memo[target] = True
+        return True
+    
+    for word in word_bank:
+        if word in target and target.index(word) == 0:
+            new_string = target[len(word):]
+            if can_construct_memo(new_string, word_bank, memo):
+                memo[target] = True
+                return True
+    
+    memo[target] = False
+    return False
--- algorithms/python/dynamicprogramming/tests/test_canConstruct.py
+++ algorithms/python/dynamicprogramming/tests/test_canConstruct.py
@@ -1,15 +1,21 @@
 import unittest
 
-from ..canConstruct import can_construct
+from ..canConstruct import can_construct, can_construct_memo
 
 
 class TestCan(unittest.TestCase):
 
     def test_can_construct(self):
-        self.assertEqual(can_construct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd']), True)
-        self.assertEqual(can_construct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']), False)
-        self.assertEqual(can_construct('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o', 't']), True)
-        self.assertEqual(can_construct('eeeeeeeeeef', ['e', 'ee', 'eee', 'eeee', 'eeeee', 'eeeeee']), False)
+        self.assertTrue(can_construct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd']))
+        self.assertFalse(can_construct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']))
+        self.assertTrue(can_construct('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o', 't']))
+        self.assertFalse(can_construct('eeeeeeeeeef', ['e', 'ee', 'eee', 'eeee', 'eeeee', 'eeeeee']))
+    
+    def test_can_construct_memo(self):
+        self.assertTrue(can_construct_memo('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd'], {}))
+        self.assertFalse(can_construct_memo('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar'], {}))
+        self.assertTrue(can_construct_memo('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o', 't'], {}))
+        self.assertFalse(can_construct_memo('eeeeeeeeeef', ['e', 'ee', 'eee', 'eeee', 'eeeee', 'eeeeee'], {}))      
 
 
 if __name__ == "__main__":
